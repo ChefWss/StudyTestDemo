01f66f98eb57bccddf822235eccc25ac591a4374
WangSS <shaoshuai.wang@yaoxinhd.com>
2020-11-23 18:28:52 +0800

    socket的sdk

--- Test_socket/Test_socket/SdkViewController.m
+++ Test_socket/Test_socket/SdkViewController.m
@@ -11,7 +11,7 @@
 
 @interface SdkViewController ()<GCDAsyncSocketDelegate>
 
-@property (nonatomic, strong) GCDAsyncSocket *clentSocket;
+@property (nonatomic, strong) GCDAsyncSocket *clientSocket;
 
 @end
 
@@ -20,20 +20,50 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    
-    self.clentSocket = [[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:dispatch_get_main_queue()];
+}
+
+
+- (IBAction)sdkCreateClick:(id)sender
+{
+    self.clientSocket = [[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:dispatch_get_main_queue()];
     
     NSString *host = @"127.0.0.1";
-    uint16_t port = htons(9999);
+    uint16_t port = htons(9998);
     
     
     NSError *err = nil;
-    if (![self.clentSocket connectToHost:host onPort:port error:&err])
+    if (![self.clientSocket connectToHost:host onPort:port error:&err])
     {
         //  如果有错误，很可能是"已经连接"或"没有委托集"
-        NSLog(@"连接错误: %@", err);
+        NSLog(@"sdk连接错误: %@", err);
     }
-    
+    else
+    {
+        NSLog(@"sdk连接成功: %@", self.clientSocket);
+    }
+}
+
+
+
+- (IBAction)sdkSendClick:(id)sender
+{
+    [self.clientSocket writeData:[@"i m sdk from" dataUsingEncoding:NSUTF8StringEncoding] withTimeout:-1 tag:0];
+}
+
+
+#pragma mark - delegate
+- (void)socket:(GCDAsyncSocket *)sock didConnectToHost:(NSString *)host port:(uint16_t)port
+{
+    NSLog(@"delegate sdk连接成功");
+    [sock readDataWithTimeout:-1 tag:0];
+}
+
+
+- (void)socket:(GCDAsyncSocket *)sock didReadData:(NSData *)data withTag:(long)tag
+{
+    NSString *recvString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    NSLog(@"delegate 服务器包返回了--回包内容: %@ ---长度%ld", recvString, data.length);
+    [sock readDataWithTimeout:-1 tag:0];
 }
 
 /*
